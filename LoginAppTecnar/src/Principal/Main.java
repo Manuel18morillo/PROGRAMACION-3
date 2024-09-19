57bc18363e61bdb235180070df29e42ea6ce5c40
Manuel Morillo <160240076+Manuel18morillo@users.noreply.github.com>
2024-09-19 11:56:02 -0500

    Add files via upload

---
+++ LoginAppTecnar/src/Principal/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import Formularios.FrmLogin;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        FrmLogin L = new FrmLogin();
+        L.setVisible(true);
+        
+    }
+    
+}
