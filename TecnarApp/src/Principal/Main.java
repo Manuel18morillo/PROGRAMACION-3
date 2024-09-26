ee46d22b9df21ba624e9cb9b4ab6110199f1f2d9
Manuel Morillo <160240076+Manuel18morillo@users.noreply.github.com>
2024-09-26 12:00:30 -0500

    Add files via upload

---
+++ TecnarApp/src/Principal/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import Ventanas.FrmLogin;
+
+/**
+ *
+ * @author Ing. Narvaez Mejia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //JfrmDatos app = new FrmAdminClientes();
+        FrmLogin app = new FrmLogin();
+        app.setVisible(true);
+    }
+    
+}
